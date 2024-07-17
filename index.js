99ac2416cdb87e9159137b65128af39edb17667f
Humberto Zambrano <elhumbertoz@gmail.com>
2024-07-17 10:41:57 -0500

    Webhook

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT_WEB = 5025; // Puerto para el servidor web
+const PORT_WEB = process.env.PORT || 3000; // Puerto para el servidor web
 
 // Aumentar el límite de tamaño del cuerpo de la solicitud
 app.use(bodyParser.json({ limit: '100mb' }));
@@ -23,24 +23,10 @@ app.post('/webhook', async (req, res) => {
             break;
         case 'media':
             // Procesar media
-            try {
-                data = await saveMediaData(data);
-            } catch (error) {
-                console.log("Error al guardar media", error);
-            }
             console.log('Media recibida:', data);
             break;
         default:
             console.log('Tipo de contenido desconocido:', content);
-    }
-
-    try {
-        saveBuffer({
-            content: content,
-            ...data
-        });
-    } catch (error) {
-        console.log("Error al guardar datos en archivo", error);
     }
 
     // Responder con éxito
